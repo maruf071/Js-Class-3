d31e4783c109597203082e6cea015cb896aaa53d
Maruf Khan <marufatrai6@gmail.com>
2023-11-08 19:04:28 +0600

    Class-3 Added

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+
+// Javascript DataType; and Comment ;
+    // String type
+    // Number type
+    // Boolean type
+
+    document.write( typeof("Maruf Khan") + "<br/>"); // it is data String type
+
+    document.write( typeof(123)+ "<br/>"); // it is Number data type
+
+    document.write( typeof(true)); // it is Boolean data type
+
+
+    // JavaScript Single line Comment 
+
+    // window.alert("Comment"); it is single line Comment;
+
+    // JavaScript Multipul  line Comment 
+
+   /*
+        console.log ("Hello");
+        console.log ("Hello");
+        console.log ("Hello");
+   */
